2da8cad262ff72bd6fb1a2d61a140fcc48a4bd0e
filipadamus <filip.adamus@onet.eu>
2023-03-08 22:16:51 +0100

    Dodanie autoryzacji

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use App\Models\Customer;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function show(Customer $customer)
+    {
+        $this->authorize('view', $customer);
+
+        return view('customers.show', compact('customer'));
+    }
 }
