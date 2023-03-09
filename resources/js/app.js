46a8fe7ebabcacbd3075f7dc1d407f66386ac2e3
filipadamus <filip.adamus@onet.eu>
2023-03-09 20:53:14 +0100

    Zadanie 17,instalcja Axios oraz edytowanie pliku app.js który pobiera dane z users w momencie tworzenia komponentu

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import axios from 'axios';
 
 Vue.use(VueRouter)
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
@@ -23,4 +25,20 @@ const router = new VueRouter({
   ]
 })
 
+new Vue({
+  router,
+  data: {
+    users: []
+  },
+  created() {
+    axios.get('/api/users')
+      .then(response => {
+        this.users = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  render: h => h(App),
+}).$mount('#app')
 
