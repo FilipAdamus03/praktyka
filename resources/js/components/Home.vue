cefe4a56b4c4121c09ba9a7871c1cd4235e15ced
filipadamus <filip.adamus@onet.eu>
2023-03-09 20:21:39 +0100

    Zadanie 16, stworzenie 2 plików vue

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h1>Strona domowa</h1>
+      <UserCard :user="user" />
+    </div>
+  </template>
+  
+  <script>
+  import UserCard from '../components/UserCard.vue'
+  
+  export default {
+    name: 'Home',
+    components: {
+      UserCard
+    },
+    data() {
+      return {
+        user: {
+          name: 'filip adamus',
+          email: 'filip.adamus@onte.eu',
+          phone: '123456789',
+          address: 'Krakow'
+        }
+      }
+    }
+  }
+  </script>
+  
